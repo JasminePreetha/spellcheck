76f0cef9010f5f91c6c96de3f63e0c1dcba128d9
Jasmine Preetha <jasminepreetha.r@gmail.com>
2024-10-30 15:50:28 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
 function App() {
+  const [input, setInput] = useState("");
+  const [val, setVal] = useState("");
+  const customDictionary = {
+    teh: "the",
+
+    wrok: "work",
+
+    fot: "for",
+
+    exampl: "example",
+  };
+  function res(input, customDictionary) {
+    let result = input.split(" ");
+    // console.log(result, "this is the splitted value");
+    for (let i = 0; i < result.length; i++) {
+      if (customDictionary[result[i].toLowerCase()]) {
+        return customDictionary[result[i].toLowerCase()];
+      }
+    }
+    return "";
+  }
+  useEffect(() => {
+    let result = res(input, customDictionary);
+    // console.log(result, "this is the returned value");
+    setVal(result);
+     // eslint-disable-next-line
+  }, [input]);
+
+  console.log(input);
+  console.log(val);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Spell Check and Auto-Correction</h1>
+      <div>
+        <textarea
+          placeholder="Enter text..."
+          rows="6"
+          cols="50"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        ></textarea>
+      </div>
+      {val ? <p>Did you mean: {val}?</p> : ""}
     </div>
   );
 }
